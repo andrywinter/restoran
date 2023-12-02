633fb3cf612a4151e377e476f03c61932de9e5ee
andrywinter <134313278+andrywinter@users.noreply.github.com>
2023-12-02 14:14:53 +0100

    commid chamges

--- js/main.js
+++ js/main.js
@@ -2,12 +2,20 @@ const button = document.querySelector(".video-btn");
 const scVideo = document.querySelector(".video");
 const video = document.querySelector("#video");
 
-button.addEventListener("click", function(){
-    button.classList.add ("active");
+button.addEventListener("click", function () {
+    button.classList.add("active");
     scVideo.classList.add("hide");
     playclip(video);
 });
 
-function playclip(media){
+function playclip(media) {
     media.play()
 }
+
+$(document).ready(function () {
+    $('.gallery-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    })
+});
